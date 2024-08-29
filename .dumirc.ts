90d3e1cedcfe36763ad025c451286d7cb54826a7
LengWeiping <lengweiping1983@hotmail.com>
2024-08-29 12:13:53 +0800

    first commit

---
+++ .dumirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  outputPath: 'docs-dist',
+  base: '/react-svg-charts/',
+  publicPath: '/react-svg-charts/',
+  themeConfig: {
+    name: 'leng',
+  },
+});
---
+++ src/Foo/index.md
@@ -0,0 +1,9 @@
+# Foo
+
+This is an example component.
+
+```jsx
+import { Foo } from 'leng';
+
+export default () => <Foo title="Hello dumi!" />
+```
